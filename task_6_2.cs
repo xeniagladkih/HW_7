86c7eb46e28d8b611fd3a8af8371ee0e27863bf3
Kseniya Gladkih <114312884+xeniagladkih@users.noreply.github.com>
2022-12-03 11:52:33 -0500

    Facade

---
+++ task_6_2.cs
@@ -0,0 +1,59 @@
+﻿//Уявімо нам необхідно розробити додаток для розміщення замовлення.\
+//Для реалізації використаємо паттерн Facade.
+
+using System;
+namespace FacadeDesignPattern
+{
+    //Creating subsystems
+
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Fetching the Product Details");
+        }
+    }
+
+    public class Payment
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Done Successfully");
+        }
+    }
+
+    public class Invoice
+    {
+        public void Sendinvoice()
+        {
+            Console.WriteLine("Invoice Send Successfully");
+        }
+    }
+
+    //Creating Facade
+    public class Order
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Place Order Started");
+            Product product = new Product();
+            product.GetProductDetails();
+            Payment payment = new Payment();
+            payment.MakePayment();
+            Invoice invoice = new Invoice();
+            invoice.Sendinvoice();
+            Console.WriteLine("Order Placed Successfully");
+        }
+    }
+
+    //Client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
